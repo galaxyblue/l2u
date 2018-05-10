4559e5da77a22cdee3d3b68c8b0e01e61d886ad5
Annie Hou <aniwho3@gmail.com>
2018-05-10 18:35:44 -0400

    finished q1, added sympathetic cmd line tag

--- wizard.py
+++ wizard.py
@@ -8,6 +8,7 @@ Created on Thu May 10 15:27:24 2018
 import tkinter as tkr
 import pyttsx3 as tts
 import time
+import sys
 
 # set up text to speech
 engine = tts.init()
@@ -15,6 +16,14 @@ voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 
 root = tkr.Tk()
+
+sympa = False;
+
+try:
+    if sys.argv[1] == "-s":
+        sympa = True 
+except(SyntaxError, IndexError):
+    pass
 
 ''' introduction and first dialogue '''
 
@@ -39,9 +48,14 @@ def smallTalkAndIntro(event):
         engine.say("all day")
     else:
         engine.say("for a while")
+    
     engine.say("It gets pretty quiet around here.")
     engine.say("Allow me to introduce myself, my name is L2U2.")
     engine.say("Thank you for taking the time to meet with me today.")
+    
+    if sympa == True:
+        engine.say("I know you are probably busy this time of year.")
+    
     engine.runAndWait()
 
 # question 1
@@ -50,36 +64,50 @@ def howAreYou(event):
     engine.runAndWait()
 
 def howResponse(howentry):
-    resp = howentry.widget.get()
-    print(resp)
-'''    
-    if listen1 == "good":
+    sympa1 = howentry.widget.get()
+
+    engine.say("I see.") 
+
+    if sympa1 == "good":
         engine.say("I'm glad you feel that way.")
-    elif listen1 == "bad":
+    elif sympa1 == "bad":
         engine.say("I'm sorry you feel that way.")
     else:
         engine.say("Ah, it's one of those days. I hope your day gets better.")
-        '''
+    
+    engine.runAndWait()
+
+def conclusion1(event):
+    engine.say("I see. I feel pretty good myself. I get to talk to you today.")
     
     
 ''' creating GUI '''
+topFrame = tkr.Frame(root)
+topFrame.pack()
+bottomFrame = tkr.Frame(root)
+bottomFrame.pack(side="bottom")
 
-introlabel = tkr.Label(root, text="Introduction", bg="black", fg="white", font=("", 20))
+introlabel = tkr.Label(topFrame, text="Exchange 1", bg="black", fg="white", font=("", 16))
 
-smalltalkintro =tkr.Button(root, text="Small Talk and Intro", font=("", 14))
+smalltalkintro =tkr.Button(bottomFrame, text="Small Talk + Intro", font=("", 14))
 smalltalkintro.bind("<Button-1>", smallTalkAndIntro)
 
-howareyou = tkr.Button(root, text="How are you?", font=("", 14))
+howareyou = tkr.Button(bottomFrame, text="How are you?", font=("", 14))
 howareyou.bind("<Button-1>", howAreYou)
 
-howlabel = tkr.Label(root, text="Good/Bad/Else", bg="gray", fg="white", font=("", 14))
-howentry = tkr.Entry(root)
-howentry.bind("<Return>", howResponse)
+if sympa == True:
+    howlabel = tkr.Label(bottomFrame, text="S1: Input good/bad/else", bg="gray", fg="white", font=("", 14))
+    howentry = tkr.Entry(bottomFrame)
+    howentry.bind("<Return>", howResponse)
+
+howareyou = tkr.Button(bottomFrame, text="How are you?", font=("", 14))
+howareyou.bind("<Button-1>", howAreYou)
 
 introlabel.grid(row=0)
 smalltalkintro.grid(row=1)
 howareyou.grid(row=2)
-howlabel.grid(row=3)
-howentry.grid(row=3, column=1)
+if sympa == True:
+    howlabel.grid(row=3)
+    howentry.grid(row=3, column=1)
 
 root.mainloop()
